1dff8a0af05eb153c0f03cb55e3da0275cda2d32
Wu Gao <wugao@codeaurora.org>
2018-03-26 10:26:00 +0800

    qcacmn: Cleanup roc and handle tx ack in schedule thread
    To avoid different thread delete the roc context or tx context at same
    time, move cleanup roc and handle tx ack in scheduler thread.
    
    Change-Id: Idc2a5394886a2e10c3acfabe139365dd263d6c0a
    CRs-Fixed: 2211827

--- umac/p2p/dispatcher/src/wlan_p2p_ucfg_api.c
+++ umac/p2p/dispatcher/src/wlan_p2p_ucfg_api.c
@@ -210,9 +210,8 @@ QDF_STATUS ucfg_p2p_roc_cancel_req(struct wlan_objmgr_psoc *soc,
 
 QDF_STATUS ucfg_p2p_cleanup_roc(struct wlan_objmgr_vdev *vdev)
 {
-	struct wlan_objmgr_psoc *psoc;
 	struct p2p_soc_priv_obj *p2p_soc_obj;
-	uint32_t vdev_id;
+	struct wlan_objmgr_psoc *psoc;
 
 	p2p_debug("vdev:%pK", vdev);
 
@@ -221,7 +220,6 @@ QDF_STATUS ucfg_p2p_cleanup_roc(struct wlan_objmgr_vdev *vdev)
 		return QDF_STATUS_E_INVAL;
 	}
 
-	vdev_id = (uint32_t)wlan_vdev_get_id(vdev);
 	psoc = wlan_vdev_get_psoc(vdev);
 	if (!psoc) {
 		p2p_err("null psoc");
@@ -235,7 +233,7 @@ QDF_STATUS ucfg_p2p_cleanup_roc(struct wlan_objmgr_vdev *vdev)
 		return QDF_STATUS_E_FAILURE;
 	}
 
-	return p2p_cleanup_roc_by_vdev(p2p_soc_obj, vdev_id);
+	return p2p_cleanup_roc_sync(p2p_soc_obj, vdev);
 }
 
 QDF_STATUS ucfg_p2p_mgmt_tx(struct wlan_objmgr_psoc *soc,
@@ -292,7 +290,6 @@ QDF_STATUS ucfg_p2p_mgmt_tx(struct wlan_objmgr_psoc *soc,
 	tx_action->no_cck = mgmt_frm->no_cck;
 	tx_action->no_ack = mgmt_frm->dont_wait_for_ack;
 	tx_action->off_chan = mgmt_frm->off_chan;
-	tx_action->is_deleting = false;
 	tx_action->buf = qdf_mem_malloc(tx_action->buf_len);
 	if (!(tx_action->buf)) {
 		p2p_err("Failed to allocate buffer for action frame");
