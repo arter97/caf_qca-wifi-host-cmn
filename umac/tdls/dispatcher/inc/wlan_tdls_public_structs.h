9ced6a51ca09abaa9d22725a0ee627086a310e39
Jingxiang Ge <jge@codeaurora.org>
2018-07-13 15:16:03 +0800

    qcacmn: Fix use after free for vdev in tdls
    When disconnect happens, TDLS_NOTIFY_STA_DISCONNECTION will be
    send to scheduler_thread, in bad case, it is processed after
    3s later, and the vdev passed in notify msg has been freed in
    the period, invalid vdev causes invalid memory access.
    
    change as below:
     1.1 Get vdev ref before send msg
     1.2 register callback for release ref
     1.3 send msg to scheduler_thread
     1.4 process tdls notify msg and call callback function.
    
    Change-Id: I2b03db8b30db623796f2e8299f14ee31e28efb91
    CRs-Fixed: 2275019

--- umac/tdls/dispatcher/inc/wlan_tdls_public_structs.h
+++ umac/tdls/dispatcher/inc/wlan_tdls_public_structs.h
@@ -567,6 +567,9 @@ typedef QDF_STATUS
 				      uint32_t vdev_param,
 				      bool is_link_up);
 
+/* This callback is to release vdev ref for tdls sta notify msg */
+typedef void (*tdls_sta_notify_callback)(struct wlan_objmgr_vdev *vdev);
+
 /**
  * struct tdls_start_params - tdls start params
  * @config: tdls user config
@@ -1049,6 +1052,7 @@ struct tdls_sta_notify_params {
 	bool lfr_roam;
 	bool user_disconnect;
 	uint8_t session_id;
+	tdls_sta_notify_callback callback;
 };
 
 /**
