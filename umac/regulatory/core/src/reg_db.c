mistersmee/android_kernel_motorola_cebu

qcacmn: APL9 Regdomain Changes for Korea
The following changes are made on APL9 regdomain:
* Include channel 149 in the supported channel list.
* Change the regulatory tx power for 5150-5250 frequency range to 23dBm.

Change-Id: I2bb01acbf60d814a2bc2c35a86f7f2c30583aad9
CRs-Fixed: 2366774

umac/regulatory/core/src/reg_db.c
@@ -1271,9 +1271,9 @@ const struct regdomain regdomains_5g[] = {
 	[APL8] = {CTL_FCC, DFS_ETSI_REGION, 2, 0, 2, {CHAN_5250_5330_4,
 						      CHAN_5735_5835_2} },
 
-	[APL9] = {CTL_ETSI, DFS_KR_REGION, 2, 6, 4, {CHAN_5170_5250_3,
+	[APL9] = {CTL_ETSI, DFS_KR_REGION, 2, 6, 4, {CHAN_5170_5250_2,
 						     CHAN_5250_5330_1,
-						     CHAN_5490_5710_1,
+						     CHAN_5490_5730_5,
 						     CHAN_5735_5835_2} },
 
 	[APL10] = {CTL_ETSI, DFS_FCC_REGION, 2, 6, 4, {CHAN_5170_5250_2,

arter97/caf_qca-wifi-host-cmn

umac/regulatory/core/src/reg_db.c
@@ -1252,10 +1252,10 @@ const struct regdomain regdomains_5g[] = {
 	[APL8] = {CTL_ETSI, DFS_ETSI_REG, 2, 0, 2, {CHAN_5250_5330_4,
 						CHAN_5735_5835_2} },
 
-	[APL9] = {CTL_ETSI, DFS_KR_REG, 2, 6, 4, {CHAN_5170_5250_3,
-					      CHAN_5250_5330_1,
-					      CHAN_5490_5710_1,
-					      CHAN_5735_5835_2} },
+	[APL9] = {CTL_ETSI, DFS_KR_REG, 2, 6, 4, {CHAN_5170_5250_2,
+						     CHAN_5250_5330_1,
+						     CHAN_5490_5730_5,
+						     CHAN_5735_5835_2} },
 
 	[APL10] = {CTL_ETSI, DFS_FCC_REG, 2, 6, 4, {CHAN_5170_5250_2,
 						 CHAN_5250_5330_4,