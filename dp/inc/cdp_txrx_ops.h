dedb33f5a5ef155e0ceb4d8b7d2681d0a9c5301d
Jia Ding <jiad@codeaurora.org>
2020-09-02 19:42:50 +0800

    qcacmn: Do smmu unmap for IPA TX and RX doorbell registers
    During driver initialization, IPA TX and RX doorbell registers
    are provided to wlan as physical addresses. With SMMU S1 enabled,
    they're mapped to IOVA of wlan domain.
    
    On driver deinit path, do smmu unmap for the two IOVA addresses.
    
    Change-Id: I85ef1c3e99bef504abf09eebf9ace760b68f35f1
    CRs-Fixed: 2768303

--- dp/inc/cdp_txrx_ops.h
+++ dp/inc/cdp_txrx_ops.h
@@ -1231,8 +1231,9 @@ struct cdp_ipa_ops {
 		uint32_t ipa_desc_size, void *ipa_priv, bool is_rm_enabled,
 		uint32_t *tx_pipe_handle, uint32_t *rx_pipe_handle);
 #endif /* CONFIG_IPA_WDI_UNIFIED_API */
-	QDF_STATUS (*ipa_cleanup)(uint32_t tx_pipe_handle,
-		uint32_t rx_pipe_handle);
+	QDF_STATUS (*ipa_cleanup)(struct cdp_pdev *ppdev,
+				  uint32_t tx_pipe_handle,
+				  uint32_t rx_pipe_handle);
 	QDF_STATUS (*ipa_setup_iface)(char *ifname, uint8_t *mac_addr,
 		qdf_ipa_client_type_t prod_client,
 		qdf_ipa_client_type_t cons_client,
