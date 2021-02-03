717febaee207d42a3d14568691ad77dd2fdc132d
Yeshwanth Sriram Guntuka <ysriramg@codeaurora.org>
2021-02-03 12:27:27 +0530

    qcacmn: Set authorize flag when peer state is updated to auth
    Set authorize flag when peer state is update to
    authenticated state.
    
    Change-Id: I303bb9e32f72176e73bbdc2861468cf0fd1a43c6
    CRs-Fixed: 2869537

--- dp/wifi3.0/dp_peer.c
+++ dp/wifi3.0/dp_peer.c
@@ -3244,7 +3244,9 @@ QDF_STATUS dp_peer_state_update(struct cdp_pdev *pdev_handle, uint8_t *peer_mac,
 	}
 	peer->state = state;
 
+	peer->authorize = (state == OL_TXRX_PEER_STATE_AUTH) ? 1 : 0;
 	dp_info("peer %pK state %d", peer, peer->state);
+
 	/* ref_cnt is incremented inside dp_peer_find_hash_find().
 	 * Decrement it here.
 	 */
