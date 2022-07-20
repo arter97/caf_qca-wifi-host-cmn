MiCode/vendor_qcom_opensource_wlan

qcacmn: Add support for TX stats in IPA use case
Add support for TX stats in IPA use case

Change-Id: Ie7d2c6d27219c49dc09bcacc65851b0c37fc337e
CRs-Fixed: 3231035

dp/wifi3.0/monitor/dp_mon.c
@@ -2685,6 +2685,7 @@ dp_tx_stats_update(struct dp_pdev *pdev, struct dp_peer *peer,
 	enum cdp_ru_index ru_index;
 	struct dp_mon_peer *mon_peer = NULL;
 	uint32_t ratekbps = 0;
+	uint64_t tx_byte_count;
 
 	preamble = ppdu->preamble;
 	mcs = ppdu->mcs;
@@ -2692,6 +2693,7 @@ dp_tx_stats_update(struct dp_pdev *pdev, struct dp_peer *peer,
 	num_mpdu = ppdu->mpdu_success;
 	mpdu_tried = ppdu->mpdu_tried_ucast + ppdu->mpdu_tried_mcast;
 	mpdu_failed = mpdu_tried - num_mpdu;
+	tx_byte_count = ppdu->success_bytes;
 
 	/* If the peer statistics are already processed as part of
 	 * per-MSDU completion handler, do not process these again in per-PPDU
@@ -2703,6 +2705,12 @@ dp_tx_stats_update(struct dp_pdev *pdev, struct dp_peer *peer,
 	mon_peer = peer->monitor_peer;
 	if (!mon_peer)
 		return;
+
+	if (!ppdu->is_mcast) {
+		DP_STATS_INC(mon_peer, tx.tx_ucast_total.num, num_msdu);
+		DP_STATS_INC(mon_peer, tx.tx_ucast_total.bytes,
+			     tx_byte_count);
+	}
 
 	if (ppdu->completion_status != HTT_PPDU_STATS_USER_STATUS_OK) {
 		/*
@@ -2769,6 +2777,12 @@ dp_tx_stats_update(struct dp_pdev *pdev, struct dp_peer *peer,
 	DP_STATS_INCC(mon_peer, tx.ldpc, num_msdu, ppdu->ldpc);
 	if (!(ppdu->is_mcast) && ppdu->ack_rssi_valid)
 		DP_STATS_UPD(mon_peer, tx.last_ack_rssi, ack_rssi);
+
+	if (!ppdu->is_mcast) {
+		DP_STATS_INC(mon_peer, tx.tx_ucast_success.num, num_msdu);
+		DP_STATS_INC(mon_peer, tx.tx_ucast_success.bytes,
+			     tx_byte_count);
+	}
 
 	DP_STATS_INCC(mon_peer,
 		      tx.pkt_type[preamble].mcs_count[MAX_MCS - 1], num_msdu,

arter97/caf_qca-wifi-host-cmn

dp/wifi3.0/monitor/dp_mon.c
@@ -1576,6 +1576,7 @@ dp_tx_stats_update(struct dp_pdev *pdev, struct dp_peer *peer,
 	uint16_t num_mpdu;
 	uint16_t mpdu_tried;
 	uint16_t mpdu_failed;
+	uint64_t tx_byte_count;
 
 	preamble = ppdu->preamble;
 	mcs = ppdu->mcs;
@@ -1583,6 +1584,7 @@ dp_tx_stats_update(struct dp_pdev *pdev, struct dp_peer *peer,
 	num_mpdu = ppdu->mpdu_success;
 	mpdu_tried = ppdu->mpdu_tried_ucast + ppdu->mpdu_tried_mcast;
 	mpdu_failed = mpdu_tried - num_mpdu;
+	tx_byte_count = ppdu->success_bytes;
 
 	/* If the peer statistics are already processed as part of
 	 * per-MSDU completion handler, do not process these again in per-PPDU
@@ -1590,7 +1592,10 @@ dp_tx_stats_update(struct dp_pdev *pdev, struct dp_peer *peer,
 	 */
 	if (pdev->soc->process_tx_status)
 		return;
-
+	if (!ppdu->is_mcast) {
+		DP_STATS_INC(peer, tx.tx_ucast_total.num, num_msdu);
+		DP_STATS_INC(peer, tx.tx_ucast_total.bytes, tx_byte_count);
+	}
 	if (ppdu->completion_status != HTT_PPDU_STATS_USER_STATUS_OK) {
 		/*
 		 * All failed mpdu will be retried, so incrementing
@@ -1678,6 +1683,10 @@ dp_tx_stats_update(struct dp_pdev *pdev, struct dp_peer *peer,
 	 * ack failure i.e for long retries we get
 	 * mpdu failed equal mpdu tried.
 	 */
+	if(!ppdu->is_mcast) {
+		DP_STATS_INC(peer, tx.tx_ucast_success.num, num_msdu);
+		DP_STATS_INC(peer, tx.tx_ucast_success.bytes, tx_byte_count);
+	}
 	DP_STATS_INC(peer, tx.retries, mpdu_failed);
 	DP_STATS_INC(peer, tx.tx_failed, ppdu->failed_msdus);
 