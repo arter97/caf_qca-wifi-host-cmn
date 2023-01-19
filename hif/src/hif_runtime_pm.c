049de5567ee10aadff5f08ac62d6a85eed11e782
Rajeev Kumar <quic_rajekuma@quicinc.com>
2023-01-18 16:14:29 -0800

    qcacmn: Add new line after WoW wake lock log stats
    Add new line after WoW wake lock log to assist in debugging
    by keeping new stats separate from old stats.
    
    Change-Id: I818635a0afc70c82bf1a6b8e8b4e57c116d2a7e6
    CRs-Fixed: 3383789

--- hif/src/hif_runtime_pm.c
+++ hif/src/hif_runtime_pm.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2013-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -170,6 +170,7 @@ static int hif_rtpm_debugfs_show(struct seq_file *s, void *data)
 		seq_puts(s, " ");
 	}
 	qdf_spin_unlock_bh(&gp_hif_rtpm_ctx->prevent_list_lock);
+	seq_puts(s, "\n");
 
 	return 0;
 }
