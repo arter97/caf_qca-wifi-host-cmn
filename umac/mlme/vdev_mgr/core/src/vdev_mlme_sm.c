575daf29552cebbab2f9799c58d1aacf7fafc708
Surya Prakash Sivaraj <quic_suryapra@quicinc.com>
2023-02-03 05:47:00 -0800

    qcacmn: Cleanup link vdev SM on roam HO failure
    In legacy to MLO roaming case, the link vdev will be
    moved from INIT->UP via EV_ROAM event. Similarly, if
    the Hand-off fails in host due to host-related handling,
    treat the failure as HO-FAILURE and move the link VDEV
    SM from UP->INIT directly.
    
    Change-Id: Ia6bfeb958f0302a934d24c9b40fadd4a8f557236
    CRs-Fixed: 3389148

--- umac/mlme/vdev_mgr/core/src/vdev_mlme_sm.c
+++ umac/mlme/vdev_mgr/core/src/vdev_mlme_sm.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2018-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -153,6 +153,7 @@ static bool mlme_vdev_state_init_event(void *ctx, uint16_t event,
 	struct vdev_mlme_obj *vdev_mlme = (struct vdev_mlme_obj *)ctx;
 	bool status;
 	enum QDF_OPMODE mode;
+	QDF_STATUS sm_status;
 
 	mode = wlan_vdev_mlme_get_opmode(vdev_mlme->vdev);
 
@@ -208,9 +209,17 @@ static bool mlme_vdev_state_init_event(void *ctx, uint16_t event,
 		 */
 		if (wlan_vdev_mlme_is_mlo_link_vdev(vdev_mlme->vdev)) {
 			mlme_vdev_sm_transition_to(vdev_mlme, WLAN_VDEV_S_UP);
-			mlme_vdev_sm_deliver_event(vdev_mlme, event,
-						   event_data_len, event_data);
-			status = true;
+			sm_status = mlme_vdev_sm_deliver_event(vdev_mlme, event,
+							       event_data_len,
+							       event_data);
+			status = !sm_status;
+			/*
+			 * Error in handling link-vdev roam event, move the
+			 * SM back to INIT.
+			 */
+			if (QDF_IS_STATUS_ERROR(sm_status))
+				mlme_vdev_sm_transition_to(vdev_mlme,
+							   WLAN_VDEV_S_INIT);
 		} else {
 			status = false;
 		}
@@ -456,6 +465,7 @@ static bool mlme_vdev_state_up_event(void *ctx, uint16_t event,
 	enum QDF_OPMODE mode;
 	struct wlan_objmgr_vdev *vdev;
 	bool status;
+	QDF_STATUS sm_status;
 
 	vdev = vdev_mlme->vdev;
 	mode = wlan_vdev_mlme_get_opmode(vdev);
@@ -497,9 +507,10 @@ static bool mlme_vdev_state_up_event(void *ctx, uint16_t event,
 		if (wlan_vdev_mlme_is_mlo_link_vdev(vdev_mlme->vdev)) {
 			mlme_vdev_sm_transition_to(vdev_mlme,
 						   WLAN_VDEV_SS_UP_ACTIVE);
-			mlme_vdev_sm_deliver_event(vdev_mlme, event,
-						   event_data_len, event_data);
-			status = true;
+			sm_status = mlme_vdev_sm_deliver_event(vdev_mlme, event,
+							       event_data_len,
+							       event_data);
+			status = !sm_status;
 		} else {
 			status = false;
 		}
@@ -1783,6 +1794,7 @@ static bool mlme_vdev_subst_up_active_event(void *ctx, uint16_t event,
 	enum QDF_OPMODE mode;
 	struct wlan_objmgr_vdev *vdev;
 	bool status;
+	QDF_STATUS sm_status;
 
 	vdev = vdev_mlme->vdev;
 	mode = wlan_vdev_mlme_get_opmode(vdev);
@@ -1861,9 +1873,10 @@ static bool mlme_vdev_subst_up_active_event(void *ctx, uint16_t event,
 		break;
 
 	case WLAN_VDEV_SM_EV_ROAM:
-		mlme_vdev_notify_roam_start(vdev_mlme, event_data_len,
-					    event_data);
-		status = true;
+		sm_status = mlme_vdev_notify_roam_start(vdev_mlme,
+							event_data_len,
+							event_data);
+		status = !sm_status;
 		break;
 
 	default:
