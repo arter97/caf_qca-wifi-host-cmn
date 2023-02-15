4a9e8d65c2ab937c3a6c75e5ac4f880f3f243827
Surya Prakash Sivaraj <quic_suryapra@quicinc.com>
2023-02-15 12:35:11 +0530

    qcacmn: Add support for AKM25
    Add support for FT-SAE-EXT-KEY AKM.
    
    Change-Id: I832b414b31d42113eec6d099ba4c62e76dfe6cb1
    CRs-Fixed: 3406626

--- os_if/linux/crypto/src/wlan_nl_to_crypto_params.c
+++ os_if/linux/crypto/src/wlan_nl_to_crypto_params.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2019-2020 The Linux Foundation. All rights reserved.
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -203,6 +203,13 @@ static const struct osif_akm_type_crypto_mapping
 #endif
 		.akm_suite = WLAN_AKM_SUITE_SAE_EXT_KEY,
 		.akm_type_crypto = WLAN_CRYPTO_KEY_MGMT_SAE_EXT_KEY,
+	},
+	{
+#ifndef WLAN_AKM_SUITE_FT_SAE_EXT_KEY
+#define WLAN_AKM_SUITE_FT_SAE_EXT_KEY 0x000FAC19
+#endif
+		.akm_suite = WLAN_AKM_SUITE_FT_SAE_EXT_KEY,
+		.akm_type_crypto = WLAN_CRYPTO_KEY_MGMT_FT_SAE_EXT_KEY,
 	},
 };
 
