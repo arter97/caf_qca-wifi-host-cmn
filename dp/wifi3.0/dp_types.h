8e969e5393041f8f04f12ca12e809318904db714
Kenvish Butani <quic_kbutani@quicinc.com>
2023-07-21 11:05:15 +0530

    qcacmn: Add Support for MLO device Context Struct
    Add support for below operations for MLO device
    context structure.
    1) Allocate MLO device structure
    2) Attach vdev to MLO device structure
    3) Detach vdev from MLO device structure
    4) Free MLO device structure
    
    Change-Id: Ifc282a5a04657aa618e0dac6a64d118aa8a8f422
    CRs-Fixed: 3566880

--- dp/wifi3.0/dp_types.h
+++ dp/wifi3.0/dp_types.h
@@ -298,6 +298,7 @@ enum dp_peer_state {
  * @DP_MOD_ID_UMAC_RESET:
  * @DP_MOD_ID_TX_MCAST:
  * @DP_MOD_ID_DS:
+ * @DP_MOD_ID_MLO_DEV:
  * @DP_MOD_ID_MAX:
  */
 enum dp_mod_id {
@@ -331,6 +332,7 @@ enum dp_mod_id {
 	DP_MOD_ID_UMAC_RESET,
 	DP_MOD_ID_TX_MCAST,
 	DP_MOD_ID_DS,
+	DP_MOD_ID_MLO_DEV,
 	DP_MOD_ID_MAX,
 };
 
