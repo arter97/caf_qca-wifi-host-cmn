e60c0128f8461df27bec8ac6d2c5a513b900fd50
Vinod Kumar Pirla <quic_vpirla@quicinc.com>
2023-11-17 12:22:20 -0800

    qcacmn: Add API to check reg domain supports 11be
    Check the max supported current regulatory domain's
    phy mode equals 11be or not.
    
    Change-Id: Ic4ba81d22d195248a7a1b25f3e7fa5b31093f4c6
    CRs-Fixed: 3671851

--- umac/regulatory/dispatcher/src/wlan_reg_services_api.c
+++ umac/regulatory/dispatcher/src/wlan_reg_services_api.c
@@ -769,6 +769,16 @@ QDF_STATUS wlan_reg_get_phybitmap(struct wlan_objmgr_pdev *pdev,
 	return reg_get_phybitmap(pdev, phybitmap);
 }
 
+#ifdef WLAN_FEATURE_11BE
+bool wlan_reg_phybitmap_support_11be(struct wlan_objmgr_pdev *pdev)
+{
+	enum reg_phymode cur_max_phymode;
+
+	cur_max_phymode = reg_get_max_phymode(pdev, REG_PHYMODE_MAX - 1, 0);
+	return (cur_max_phymode == REG_PHYMODE_11BE);
+}
+#endif
+
 bool wlan_reg_is_11d_scan_inprogress(struct wlan_objmgr_psoc *psoc)
 {
 	return reg_is_11d_scan_inprogress(psoc);
