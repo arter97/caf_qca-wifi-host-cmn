8a8838230fbcc6622c4a7c047e1ce06061313d49
Vijay Raj <quic_vijaraj@quicinc.com>
2023-11-20 23:44:34 -0800

    qcacmn: Add band info parameter for wmi_mgmt_params
    For TX mgmt action frames, the band info logged
    as part of connectivity logging is different from
    the band over which the frame was sent over the
    air.
    
    Add band info parameter in wmi_mgmt_params structure
    to cache the band info value received from firmware
    after TX completion.
    
    Change-Id: Ic870d96fca1a9ea35fad54dee22a69d4f8e3637b
    CRs-Fixed: 3668931

--- wmi/inc/wmi_unified_param.h
+++ wmi/inc/wmi_unified_param.h
@@ -1899,6 +1899,7 @@ struct tx_send_params {
  *  use 6 Mbps rather than 1 Mbps min rate(for 5GHz band or P2P)
  * @peer_rssi: peer RSSI value
  * @mlo_link_agnostic: if true, can send on any active link
+ * @band: TX packet band info. Refer enum wmi_mlo_band_info_t
  */
 struct wmi_mgmt_params {
 	void *tx_frame;
@@ -1916,6 +1917,7 @@ struct wmi_mgmt_params {
 	uint32_t tx_flags;
 	int8_t peer_rssi;
 	uint8_t mlo_link_agnostic;
+	uint8_t band;
 };
 
 /**
