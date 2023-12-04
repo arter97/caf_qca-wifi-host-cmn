c443127a572610dbfb2c2dc337fe903c0856c11e
Yeshwanth Sriram Guntuka <quic_ysriramg@quicinc.com>
2023-12-04 10:50:53 +0530

    qcacmn: Add new direct link datapath recovery reason
    Add new direct link datapath recovery reason code.
    
    CRs-Fixed: 3678531
    Change-Id: I5130f848634bcf6d39ed493b0e72d1bc019395ee

--- qdf/inc/qdf_types.h
+++ qdf/inc/qdf_types.h
@@ -1614,6 +1614,7 @@ enum qdf_suspend_type {
  * timed out.
  * @QDF_VDEV_ACTIVE_SER_LINK_SWITCH_TIMEOUT: Active link switch cmd in
  * serialization timed out.
+ * @QDF_DIRECT_LINK_ADSP_NMI_CRASH: ADSP NMI crash in the context of direct link
  */
 enum qdf_hang_reason {
 	QDF_REASON_UNSPECIFIED,
@@ -1658,6 +1659,7 @@ enum qdf_hang_reason {
 	QDF_VDEV_ACTIVE_SER_DISCONNECT_TIMEOUT,
 	QDF_VDEV_ACTIVE_SER_REASSOC_TIMEOUT,
 	QDF_VDEV_ACTIVE_SER_LINK_SWITCH_TIMEOUT,
+	QDF_DIRECT_LINK_ADSP_NMI_CRASH,
 };
 
 /**
