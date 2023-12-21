ec6d430ddc865124d3f2a7bf3869837f4e87b543
Shwetha G K <quic_kshwetha@quicinc.com>
2023-12-21 23:10:32 +0530

    qcacmn: Add hal ring for wifi radar
    Add ring to support wifi radar feature
    
    CRs-Fixed: 3692473
    Change-Id: Ib5087a5e1a5d8ac417ee9908448cb92fd219c292

--- hal/wifi3.0/qcn9224/v2/hal_9224v2.c
+++ hal/wifi3.0/qcn9224/v2/hal_9224v2.c
@@ -425,10 +425,10 @@ struct hal_hw_srng_config hw_srng_table_9224v2[] = {
 
 	{ /* DIR_BUF_RX_DMA_SRC */
 		.start_ring_id = HAL_SRNG_DIR_BUF_RX_SRC_DMA_RING,
-		/* one ring for spectral, one ring for cfr and
-		 * another one ring for txbf cv upload
+		/* one ring for spectral, one ring for cfr, one ring for txbf,
+		 * another one ring for  wifi radar
 		 */
-		.max_rings = 3,
+		.max_rings = 4,
 		.entry_size = 2,
 		.lmac_ring = TRUE,
 		.ring_dir = HAL_SRNG_SRC_RING,
