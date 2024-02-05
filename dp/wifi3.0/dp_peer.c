5c09c6f7518d61f6f79c7e4ed770ebf512de5dac
Ananya Gupta <quic_anangupt@quicinc.com>
2024-02-05 10:56:15 +0530

    qcacmn: Validate rx tid when requesting stat from HW
    Crash is observed as rx_tid was null when HW stats were
    requested.
    Adding a NULL check on peer->rx_tid to avoid NULL dereference.
    
    Change-Id: I572464d93780fb5080d9bc8060e4d22bff6356b7
    CRs-Fixed: 3688412

--- dp/wifi3.0/dp_peer.c
+++ dp/wifi3.0/dp_peer.c
@@ -5938,7 +5938,7 @@ int dp_peer_get_rxtid_stats_ipa(struct dp_peer *peer,
 			continue;
 
 		rx_tid = &peer->rx_tid[i];
-		if (rx_tid->hw_qdesc_vaddr_unaligned) {
+		if (rx_tid && rx_tid->hw_qdesc_vaddr_unaligned) {
 			params.std.need_status = 1;
 			params.std.addr_lo =
 				rx_tid->hw_qdesc_paddr & 0xffffffff;
@@ -6002,7 +6002,7 @@ int dp_peer_rxtid_stats(struct dp_peer *peer,
 			continue;
 
 		rx_tid = &peer->rx_tid[i];
-		if (rx_tid->hw_qdesc_vaddr_unaligned) {
+		if (rx_tid && rx_tid->hw_qdesc_vaddr_unaligned) {
 			params.std.need_status = 1;
 			params.std.addr_lo =
 				rx_tid->hw_qdesc_paddr & 0xffffffff;
