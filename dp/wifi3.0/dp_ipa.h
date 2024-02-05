b019f3000d9071272a3af3ad61b60d3ea5104fce
Manikanta Pubbisetty <quic_mpubbise@quicinc.com>
2024-02-05 15:17:38 +0530

    qcacmn: Optimize force free logic in dp_find_missing_tx_comp()
    Currently during runtime suspend, in dp_find_missing_tx_comp(),
    TX descriptors are freed forcefully if the TX completions for
    those descriptors do not arrive within 60 seconds.
    
    In certain rare corner cases, there is a chance of TX buffer (that is
    in the enqueue path) getting freed in the TX completion path due to
    delayed completions. This results in NULL pointer dereference.
    Following is the sequence of events for such a case,
    
    1. dp_find_missing_tx_comp() frees a TX descriptor
    2. Different buffer gets attached to the same TX descriptor
    3. Delayed completion for the previous TX arrives and frees
       the buffer in point 2.
    
    Defer the free in dp_find_missing_tx_comp() when there is a delta in
    HP/TP for the TX/COMP rings. If HP & TP are not same, then there is
    high chance of processing the delayed completion beforehand, thus
    avoiding the aforementioned race.
    
    Change-Id: Ia835928f85ea0f79d0187a55333cb8959d0a72e9
    CRs-Fixed: 3721341

--- dp/wifi3.0/dp_ipa.h
+++ dp/wifi3.0/dp_ipa.h
@@ -589,6 +589,16 @@ QDF_STATUS dp_ipa_update_peer_rx_stats(struct cdp_soc_t *soc, uint8_t vdev_id,
  * Return: None
  */
 void dp_ipa_get_wdi_version(struct cdp_soc_t *soc_hdl, uint8_t *wdi_ver);
+
+/**
+ * dp_ipa_is_ring_ipa_tx() - Check if the TX ring is used by IPA
+ *
+ * @soc: DP SoC
+ * @ring_id: TX ring id
+ *
+ * Return: bool
+ */
+bool dp_ipa_is_ring_ipa_tx(struct dp_soc *soc, uint8_t ring_id);
 #else
 static inline int dp_ipa_uc_detach(struct dp_soc *soc, struct dp_pdev *pdev)
 {
@@ -691,6 +701,12 @@ static inline QDF_STATUS dp_ipa_ast_create(struct cdp_soc_t *soc_hdl,
 static inline void dp_ipa_get_wdi_version(struct cdp_soc_t *soc_hdl,
 					  uint8_t *wdi_ver)
 {
+}
+
+static inline bool
+dp_ipa_is_ring_ipa_tx(struct dp_soc *soc, uint8_t ring_id)
+{
+	return false;
 }
 #endif
 #endif /* _DP_IPA_H_ */
