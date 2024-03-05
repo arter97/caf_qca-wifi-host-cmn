2c90c701cf49832d19c8ea5561199f2437ef69ba
Chaoli Zhou <quic_zchaoli@quicinc.com>
2024-03-05 17:34:19 +0800

    qcacmn: Support setting mcast vdev flag to dp side
    For MLO SAP case, whatever it is single link or
    multi-link, we expect that it can always follow
    up with global sequence number for broadcast/
    multicast packet.
    
    Then it needs the mcast vdev flag set in the DP side
    to distinguish if it is mlo sap or legacy sap,
    and also dp side will use this vdev as the primary
    vdev doing data forwarding.
    So add the logic to set this flag if the first
    mlo sap link to be added.
    
    Change-Id: I55a22fe224fc0044c072443915302e8e75399bfb
    CRs-Fixed: 3750541

--- umac/mlo_mgr/src/wlan_mlo_mgr_ap.c
+++ umac/mlo_mgr/src/wlan_mlo_mgr_ap.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -30,6 +30,9 @@
 #include "wlan_mlo_mgr_peer.h"
 #include "wlan_mlo_mgr_setup.h"
 
+#ifdef WLAN_FEATURE_11BE_MLO
+#include <cdp_txrx_ctrl.h>
+#endif
 #ifdef WLAN_MLO_MULTI_CHIP
 bool mlo_ap_vdev_attach(struct wlan_objmgr_vdev *vdev,
 			uint8_t link_id,
@@ -83,6 +86,40 @@ bool mlo_ap_vdev_attach(struct wlan_objmgr_vdev *vdev,
 	return true;
 }
 #else
+
+#if defined(WLAN_FEATURE_MULTI_LINK_SAP) && defined(WLAN_MCAST_MLO_SAP)
+static void
+wlan_mlme_mlo_set_mcast_vdev(struct wlan_objmgr_vdev *vdev,
+			     bool mcast_vdev)
+{
+	ol_txrx_soc_handle soc_txrx_handle;
+	struct wlan_objmgr_psoc *psoc;
+	uint8_t vdev_id;
+	cdp_config_param_type val = {0};
+
+	vdev_id = wlan_vdev_get_id(vdev);
+	psoc = wlan_vdev_get_psoc(vdev);
+	soc_txrx_handle = wlan_psoc_get_dp_handle(psoc);
+
+	if (mcast_vdev)
+		wlan_vdev_mlme_feat_ext2_cap_set(vdev,
+						 WLAN_VDEV_FEXT2_MLO_MCAST);
+	else
+		wlan_vdev_mlme_feat_ext2_cap_clear(vdev,
+						   WLAN_VDEV_FEXT2_MLO_MCAST);
+
+	val.cdp_vdev_param_mcast_vdev = mcast_vdev;
+	cdp_txrx_set_vdev_param(soc_txrx_handle, vdev_id,
+				CDP_SET_MCAST_VDEV, val);
+}
+#else
+static void
+wlan_mlme_mlo_set_mcast_vdev(struct wlan_objmgr_vdev *vdev,
+			     bool mcast_vdev)
+{
+}
+#endif
+
 bool mlo_ap_vdev_attach(struct wlan_objmgr_vdev *vdev,
 			uint8_t link_id,
 			uint16_t vdev_count)
@@ -106,6 +143,11 @@ bool mlo_ap_vdev_attach(struct wlan_objmgr_vdev *vdev,
 	dev_ctx->ap_ctx->num_ml_vdevs = vdev_count;
 	mlo_dev_lock_release(dev_ctx);
 
+	/* set the flag only for first link */
+	if (dev_ctx->wlan_vdev_count == 1) {
+		mlo_debug("set mcast flag for vdev %d", wlan_vdev_get_id(vdev));
+		wlan_mlme_mlo_set_mcast_vdev(vdev, true);
+	}
 	return true;
 }
 #endif
