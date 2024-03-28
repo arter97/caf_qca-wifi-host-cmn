b4f5316ea862f210e42b3d142f8b6dd6d56137c5
Prasanna JS <quic_pjs@quicinc.com>
2024-03-27 23:32:38 -0700

    qcacmn: Add API to write stats to chipset stats buffer
    Add API to write stats to chipset stats buffer
    
    Change-Id: Icf7de8a6925aa177443ee9f9b5b17183f36d93de
    CRs-Fixed: 3782329

--- umac/cp_stats/core/src/wlan_cp_stats_chipset_stats.c
+++ umac/cp_stats/core/src/wlan_cp_stats_chipset_stats.c
@@ -92,4 +92,84 @@ void wlan_cp_stats_cstats_register_tx_rx_ops(struct cstats_tx_rx_ops *ops)
 {
 	cstats.ops.cstats_send_data_to_usr = ops->cstats_send_data_to_usr;
 }
+
+/* Need to call this with spin_lock acquired */
+static void wlan_cp_stats_get_cstats_free_node(enum cstats_types type)
+{
+	qdf_list_node_t *tmp_node = NULL;
+
+	if (cstats.ccur_node[type]->filled_length) {
+		qdf_list_insert_back(&cstats.cstat_filled_list[type],
+				     &cstats.ccur_node[type]->node);
+	} else {
+		return;
+	}
+
+	if (!qdf_list_empty(&cstats.cstat_free_list[type])) {
+		qdf_list_remove_front(&cstats.cstat_free_list[type], &tmp_node);
+		cstats.ccur_node[type] =
+			qdf_container_of(tmp_node, struct cstats_node, node);
+	} else if (!qdf_list_empty(&cstats.cstat_filled_list[type])) {
+		qdf_list_remove_front(&cstats.cstat_filled_list[type],
+				      &tmp_node);
+		cstats.ccur_node[type] =
+			qdf_container_of(tmp_node, struct cstats_node, node);
+		qdf_err("Dropping a chipset stats node : count %d",
+			++(cstats.cstat_drop_cnt[type]));
+	}
+
+	/* Reset the current node values */
+	cstats.ccur_node[type]->filled_length = 0;
+}
+
+void wlan_cp_stats_cstats_write_to_buff(enum cstats_types type,
+					void *to_be_sent,
+					uint32_t plen)
+{
+	char *ptr;
+	unsigned int *pfilled_length;
+	unsigned int tlen;
+
+	/* tAniNlHdr + Start Marker + End Marker */
+	tlen = sizeof(tAniNlHdr) + (2 * CSTATS_MARKER_SZ);
+
+	if ((tlen + plen) > MAX_CSTATS_NODE_LENGTH) {
+		qdf_err("The Buffer is too long");
+		return;
+	}
+
+	if (!cstats.ccur_node[type]) {
+		qdf_err("Current Node is NULL");
+		return;
+	}
+
+	qdf_spin_lock_bh(&cstats.cstats_lock[type]);
+
+	pfilled_length = &cstats.ccur_node[type]->filled_length;
+
+	/* Check if we can accommodate more log into current node/buffer */
+	if ((MAX_CSTATS_NODE_LENGTH - *pfilled_length) < (tlen + plen)) {
+		wlan_cp_stats_get_cstats_free_node(type);
+		pfilled_length = &cstats.ccur_node[type]->filled_length;
+	}
+
+	if (type == CSTATS_HOST_TYPE) {
+		/* Marker will be added while flushing to userspace*/
+		ptr = &cstats.ccur_node[type]->logbuf[sizeof(tAniHdr) +
+						      CSTATS_MARKER_SZ];
+		memcpy(&ptr[*pfilled_length], to_be_sent, plen);
+		*pfilled_length += plen;
+	} else if (type == CSTATS_FW_TYPE) {
+		ptr = &cstats.ccur_node[type]->logbuf[sizeof(tAniHdr)];
+		memcpy(&ptr[*pfilled_length], CSTATS_FW_START_MARKER,
+		       CSTATS_MARKER_SZ);
+		memcpy(&ptr[*pfilled_length + CSTATS_MARKER_SZ], to_be_sent,
+		       plen);
+		memcpy(&ptr[*pfilled_length + CSTATS_MARKER_SZ + plen],
+		       CSTATS_FW_END_MARKER, CSTATS_MARKER_SZ);
+		*pfilled_length += (plen + 2 * CSTATS_MARKER_SZ);
+	}
+
+	qdf_spin_unlock_bh(&cstats.cstats_lock[type]);
+}
 #endif /* WLAN_CHIPSET_STATS */
