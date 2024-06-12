bf389476649d4199f259e59558c189749f103fe3
Abhishek Singh <quic_absingh@quicinc.com>
2024-06-12 12:57:22 +0530

    qcacmn: Add API to dump mgmt frame in hex
    Add API to dump mgmt frame in hex
    
    Change-Id: I444ed40714184d14025a72aa8a08856a6dae3a68
    CRs-Fixed: 3841017

--- umac/cmn_services/mgmt_txrx/dispatcher/inc/wlan_mgmt_txrx_utils_api.h
+++ umac/cmn_services/mgmt_txrx/dispatcher/inc/wlan_mgmt_txrx_utils_api.h
@@ -61,6 +61,16 @@
 	QDF_TRACE_DEBUG_NO_FL(QDF_MODULE_ID_MGMT_TXRX, params)
 
 /**
+ * mgmt_txrx_frame_hex_dump() - Print the type and dump the rx tx frame
+ * @frame_data: The base address of the mgmt frame data to be logged.
+ * @frame_len: The size of the frame to be logged.
+ * @is_tx: is tx frame
+ *
+ * Return:  None
+ */
+void mgmt_txrx_frame_hex_dump(void *frame_data, int frame_len, bool is_tx);
+
+/**
  * enum mgmt_subtype - enum of mgmt. subtypes
  * @MGMT_SUBTYPE_ASSOC_REQ:       association request frame
  * @MGMT_SUBTYPE_ASSOC_RESP:      association response frame
