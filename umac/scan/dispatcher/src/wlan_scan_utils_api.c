dadd833d17c2834df2d6a13807846ac2e7302a83
Tianzhen Wang <quic_tianzhen@quicinc.com>
2024-06-24 20:27:34 -0700

    Revert "qcacmn: Add check to avoid NULL pointer deference in par..."
    Revert submission 5278164
    
    Reason for revert: <revert to solve the upmerge conflict with LE.UM.6.3.6 AU642>
    Reverted Changes:
    Iad7448868:qcacmn: Fix use-after-free issue in util_scan_pars...
    I2c7a7641b:qcacmn: Fix potential OOB read in util_scan_parse_...
    Ibb9739d6b:qcacmn: Add check to avoid NULL pointer deference ...
    I6b93ae0be:qcacmn: Fix memleak in MBSSIE handler
    
    Change-Id: I85b9abea896e862bdebad1cce544a1730a58a547

--- umac/scan/dispatcher/src/wlan_scan_utils_api.c
+++ umac/scan/dispatcher/src/wlan_scan_utils_api.c
@@ -2815,8 +2815,6 @@ static QDF_STATUS util_scan_parse_mbssid(struct wlan_objmgr_pdev *pdev,
 			}
 
 			if (mbssid_info.split_prof_continue) {
-				if (!split_prof_start)
-					break;
 				nontx_profile = split_prof_start;
 				subie_len = split_prof_len;
 			} else {
@@ -2834,7 +2832,6 @@ static QDF_STATUS util_scan_parse_mbssid(struct wlan_objmgr_pdev *pdev,
 					qdf_mem_free(split_prof_start);
 					split_prof_start = NULL;
 					split_prof_end = NULL;
-					split_prof_len = 0;
 				}
 				continue;
 			}
@@ -2900,7 +2897,6 @@ static QDF_STATUS util_scan_parse_mbssid(struct wlan_objmgr_pdev *pdev,
 					qdf_mem_free(split_prof_start);
 					split_prof_start = NULL;
 					split_prof_end = NULL;
-					split_prof_len = 0;
 					qdf_mem_zero(&mbssid_info,
 						     sizeof(mbssid_info));
 				}
@@ -2915,7 +2911,6 @@ static QDF_STATUS util_scan_parse_mbssid(struct wlan_objmgr_pdev *pdev,
 				qdf_mem_free(split_prof_start);
 				split_prof_start = NULL;
 				split_prof_end = NULL;
-				split_prof_len = 0;
 			}
 			qdf_mem_free(new_frame);
 		}
