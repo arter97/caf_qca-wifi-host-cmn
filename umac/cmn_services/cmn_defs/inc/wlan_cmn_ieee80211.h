943b51f10521801da2ff75a6638b7b3662eaf63f
Akshay Mohite <mohite@qti.qualcomm.com>
2026-05-07 12:40:19 +0530

    qcacmn: Updating maximum length size for EXT capabilities
    In the current implementation, the maximum length for the
    size of the Extended Capabilities IE element is being
    restricted to 15, which is leading the driver to reject
    beacon IEs having an extended capabilities length of 255,
    which is a valid size according to the 802.11 specification.
    
    To fix this issue, this change is updating the value of
    WLAN_EXTCAP_IE_MAX_LEN to 255.
    
    CRs-Fixed: 4526830
    Change-Id: I03eb263ad9c90b005f85f2d89471cb3e64afba42

--- umac/cmn_services/cmn_defs/inc/wlan_cmn_ieee80211.h
+++ umac/cmn_services/cmn_defs/inc/wlan_cmn_ieee80211.h
@@ -343,7 +343,7 @@ enum rsn_element_identifier {
 #define WLAN_SECCHANOFF_IE_MAX_LEN               1
 #define WLAN_EXT_SUPPORTED_RATES_IE_MAX_LEN      12
 
-#define WLAN_EXTCAP_IE_MAX_LEN                   15
+#define WLAN_EXTCAP_IE_MAX_LEN                   255
 #define WLAN_FILS_INDICATION_IE_MIN_LEN          2
 #define WLAN_MOBILITY_DOMAIN_IE_MAX_LEN          3
 #define WLAN_OPMODE_IE_MAX_LEN                   1
